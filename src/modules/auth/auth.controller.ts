694ccae3720558dd50fa0ed385eaa7b3a9bb4bac
muradRajab <mgorajab@gmail.com>
2025-01-26 16:31:47 +0200

    update password form

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { signInService, signUpService } from './auth.service';
+import { repo as userRepo } from '../user/user.repo';
+import bcrypt from 'bcrypt';
 
 export const signUpController = async (
     req: Request,
@@ -31,6 +33,49 @@ export const signInController = async (
             message: 'Successfully signed in',
             data: response,
         });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const changePasswordController = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const { oldPassword, newPassword } = req.body;
+        const { id } = req.params;
+        if (!id) {
+            return res.status(401).json({ message: 'Unauthorized access' });
+        }
+        if (!oldPassword || !newPassword) {
+            return res
+                .status(400)
+                .json({ message: 'Old and new passwords are required' });
+        }
+
+        const user = await userRepo.getOne(parseInt(id));
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Verify old password
+        const isMatch = await bcrypt.compare(oldPassword, user.password);
+        if (!isMatch) {
+            return res
+                .status(400)
+                .json({ message: 'Old password is incorrect' });
+        }
+
+        // Hash the new password
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        // Update password in the database
+        user.password = hashedPassword;
+        await user.save();
+
+        res.status(200).json({ message: 'Password updated successfully' });
     } catch (error) {
         next(error);
     }
--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { signInController, signUpController } from './auth.controller';
+import {
+    signInController,
+    signUpController,
+    changePasswordController,
+} from './auth.controller';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', signUpController);
 authRouter.post('/signin', signInController);
+authRouter.post('/:id/change-password', changePasswordController);
 
 export default authRouter;
